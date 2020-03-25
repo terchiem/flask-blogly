4a60d10a3a1fd104005d6d0c5ca09c0d640683ae
Genna Mergola <gennamergola@gmail.com>
2020-03-25 16:36:06 -0700

    done until testing

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 """Blogly application."""
 
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, redirect, request
 from models import db, connect_db, User
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -37,3 +37,65 @@ def show_form():
 
     return render_template('/form.html')
 
+@app.route('/users/new', methods=['POST'])
+def create_user():
+    """ adding a new user. """
+
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    image_URL = request.form.get('image_url')
+    image_URL = image_URL if image_URL else None
+
+    new_user = User(first_name=first_name, last_name=last_name, image_url=image_URL)
+    db.session.add(new_user)
+    db.session.commit()
+
+    return redirect('/users')
+
+@app.route('/users/<user_id>')
+def show_user(user_id):
+    """ show the user page """
+
+    user = User.query.get_or_404(user_id)
+
+    return render_template('/user.html',
+                            user=user)
+
+@app.route('/users/<user_id>/edit')
+def show_edit_form(user_id):
+    """ show user edit form """
+    user = User.query.get_or_404(user_id)
+
+    return render_template('/edit.html',
+                            user=user)
+
+@app.route('/users/<user_id>/edit', methods=['POST'])
+def edit_user(user_id):
+    """ edit user. """
+
+    first_name = request.form.get('first_name')
+    last_name = request.form.get('last_name')
+    image_URL = request.form.get('image_url')
+
+    user = User.query.get_or_404(user_id)
+    
+    user.first_name=first_name
+    user.last_name=last_name
+    user.image_url=image_URL
+
+    db.session.commit()
+
+
+    return redirect('/users')
+
+@app.route('/users/<user_id>/delete', methods=['POST'])
+def delete_user(user_id):
+    """ delete user. """
+
+    user = User.query.get_or_404(user_id)
+
+    db.session.delete(user)
+
+    db.session.commit()
+
+    return redirect('/users')
